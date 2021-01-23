2f7dea38e294ee634d4e1d654d3c25dcaf6c7d7a
jianhong <1685940190@qq.com>
2021-01-23 12:07:55 +0800

    yjh  zui hou cangku

---
+++ src/main/java/com/example/ZuihoucangkuApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuihoucangkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuihoucangkuApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/pojo/Apple.java
@@ -0,0 +1,10 @@
+package com.example.pojo;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("杨键弘爱吃苹果");
+	}
+
+}
